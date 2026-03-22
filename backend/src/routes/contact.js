49f7d794c036b62f36e7d1be47a76191277c2ac6
anisha <anishasriwastav07@gmail.com>
2026-03-22 23:41:54 +0530

    fix gmail

--- backend/src/routes/contact.js
+++ backend/src/routes/contact.js
@@ -18,7 +18,9 @@ router.post('/', async (req, res) => {
     await contact.save();
 
     const transporter = nodemailer.createTransport({
-      service: "gmail",
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
